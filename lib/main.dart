b2db92a96644f8bf8a74a34bab02edd36c0029bf
Yeshey <yesheysangpo@hotmail.com>
2022-12-26 21:21:01 +0000

    doesn't need the button anymore

--- lib/main.dart
+++ lib/main.dart
@@ -17,26 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: CatFactsScreen.routename,
+      initialRoute: MealChooserScreen.routename,
       routes: {
         // '/': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
 
-        CatFactsScreen.routename : (context) => CatFactsScreen(),
+        MealChooserScreen.routename : (context) => MealChooserScreen(),
       },
-      debugShowCheckedModeBanner: false, // no longuer ddebugging flag in the app!!
+      debugShowCheckedModeBanner: false, // no longuer debugging flag in the app!!
     );
   }
 }
@@ -58,31 +48,22 @@ class Meal {
   final String desert;
 }
 
-
-
-class CatFactsScreen extends StatefulWidget {
-  const CatFactsScreen({Key? key}) : super(key: key);
+class MealChooserScreen extends StatefulWidget {
+  const MealChooserScreen({Key? key}) : super(key: key);
 
   static const String routename = '/CatFactsScreen';
 
   @override
-  State<CatFactsScreen> createState() => _MyHomePageState();
+  State<MealChooserScreen> createState() => _MealChooserScreenState();
 }
 
-class _MyHomePageState extends State<CatFactsScreen> {
+class _MealChooserScreenState extends State<MealChooserScreen> {
 
   static const String _catFactsUrl = 'http://amov.servehttp.com:8080/menu'; // 'http://amov.servehttp.com:8080/menu'; //'http://0.0.0.0:8080/menu'; // 'https://catfact.ninja/facts';
 
-  Future<String> _fetchAnAsyncString() async {
-    // Simula um pedido de 5 segundos
-    await Future.delayed(const Duration(seconds: 5));
-    return Future.value('Hello world, from an aysnc call!');
-  }
-
-
   List<Meal>? _meals = [];
   bool _fetchingData = false;
-  Future<void> _fetchCatFacts() async {
+  Future<void> _fetchMeals() async {
     try {
       setState(() => _fetchingData = true);
       http.Response response = await http.get(Uri.parse(_catFactsUrl));
@@ -113,6 +94,12 @@ class _MyHomePageState extends State<CatFactsScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _fetchMeals();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -125,10 +112,6 @@ class _MyHomePageState extends State<CatFactsScreen> {
               const Text('Cafeteria Meals'),
               const SizedBox(height: 20.0),
 
-              ElevatedButton(
-                onPressed: _fetchCatFacts,
-                child: const Text('Fetch cat facts'),
-              ),
               if (_fetchingData) const CircularProgressIndicator(),
               if (!_fetchingData && _meals != null && _meals!.isNotEmpty)
                 Flexible(
